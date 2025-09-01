0e05d2e00166ac7e7c2fcd8fb927beed1698a91a
karishma1900 <helotune258@gmail.com>
2025-09-02 00:03:09 +0530

    products are creating

--- apps/frontend/seller-ui/src/app/(routes)/dashboard/editproduct/[productId]/page.tsx
+++ apps/frontend/seller-ui/src/app/(routes)/dashboard/editproduct/[productId]/page.tsx
@@ -1,5 +1,10 @@
-import CreateProductForm from "../../createproduct/CreateProductForm"; // Adjust path to your component
+// This code is correct.
+import CreateProductForm from "../../createproduct/CreateProductForm"; 
 
+// ✅ This is the corrected version
+type PageProps = {
+  params: { productId: string };
+};
 export default function EditProductPage({ params }: { params: { productId: string } }) {
   const { productId } = params;
   return <CreateProductForm productId={productId} />;
