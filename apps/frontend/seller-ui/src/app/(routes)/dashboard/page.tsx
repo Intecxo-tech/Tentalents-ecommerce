4970dc85fe48381e3a3dd855b1411458ca53d4a2
karishma1900 <helotune258@gmail.com>
2025-09-01 23:10:24 +0530

    orders are dipslaying signup vendor is done

--- apps/frontend/seller-ui/src/app/(routes)/dashboard/page.tsx
+++ apps/frontend/seller-ui/src/app/(routes)/dashboard/page.tsx
@@ -2,22 +2,76 @@
 import HeaderBanner from '../../../shared/components/header/headerbanner'
 
 import HomeProducts from '../../../shared/components/ProductsSold/HomeProducts';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductSold from '../../../shared/components/ProductsSold/product_soldperformance/ProductSold';
 import ProductTabs from '../../../shared/components/ProductTabs/ProductTabs'
 import InventorySection from '../../../shared/components/InventorySection/InventorySection';
 import Balance from '../../../shared/components/balance/Balance';
 import ProductHistory from '../../../shared/components/ProductHistory/ProductHistory';
 import './page.css'
+import axios from 'axios';
 
+interface VendorOrder {
+  id: string;
+  quantity: number;
+  totalPrice: string;
+  dispatchStatus: string;
+  
+  order?: {
+    id: string;
+    status: string;
+    createdAt?: string;
+  };
+}
 const page = () => {
+    const [productCount, setProductCount] = useState<number>(0);
+    const [orders, setOrders] = useState<VendorOrder[]>([]);
+  
+    useEffect(() => {
+      const token = localStorage.getItem('token');
+  
+      async function fetchVendorProducts() {
+        try {
+          const res = await axios.get(
+            'https://product-service-23pc.onrender.com/api/products/vendor/products',
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          setProductCount(res.data.data.length);
+        } catch (error) {
+          console.error('Failed to fetch products.');
+        }
+      }
+    async function fetchVendorOrders() {
+      try {
+        const res = await axios.get(
+          'https://order-service-faxh.onrender.com/api/orders/vendor/orders',
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setOrders(res.data.data);
+      } catch (err) {
+        console.error('Failed to fetch orders.');
+      }
+    }
+
+    fetchVendorProducts();
+    fetchVendorOrders();
+  }, []);
+
   return (
     <div className='background-[var(--lightblue2)] p-[10px] rounded-[10px] main-pagesettings' >
      
 
    <div className="topsection flex justify-between gap-[10px] background-[white] p-[10px] rounded-[10px] mb-[15px]">
   <div className=" producthistorts w-1/3">
-    <Balance />
+    <Balance orders={orders} />
   </div>
   <div className=" producthistorts w-1/3">
     <HomeProducts />
