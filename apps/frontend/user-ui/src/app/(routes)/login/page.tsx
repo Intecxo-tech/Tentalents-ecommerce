692a63d4ffbaf4c9a2a094310e5d353b73165189
Karishma Khatri <114894267+KNkarishma2000@users.noreply.github.com>
2025-09-29 10:58:24 +0530

    Update page.tsx

--- apps/frontend/user-ui/src/app/(routes)/login/page.tsx
+++ apps/frontend/user-ui/src/app/(routes)/login/page.tsx
@@ -1,16 +1,15 @@
 import React, { Suspense } from 'react';
-import LoginClient from './LoginClient'; // Import your new client component
+import LoginClient from './LoginClient';
 
-// A simple loading component to show as a fallback
 const LoadingFallback = () => {
   return <div>Loading...</div>;
 };
 
 const Page = () => {
   return (
-
+    <Suspense fallback={<LoadingFallback />}>
       <LoginClient />
-   
+    </Suspense>
   );
 };
 
