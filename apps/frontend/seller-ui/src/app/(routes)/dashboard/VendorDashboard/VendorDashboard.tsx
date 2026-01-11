b5eeca74455ebddea8f1b834b80b2aeb10df191d
Intecxo-tech <intecxo@gmail.com>
2026-01-11 22:42:46 +0530

    Update VendorDashboard.tsx

--- apps/frontend/seller-ui/src/app/(routes)/dashboard/VendorDashboard/VendorDashboard.tsx
+++ apps/frontend/seller-ui/src/app/(routes)/dashboard/VendorDashboard/VendorDashboard.tsx
@@ -146,7 +146,7 @@ const [balanceLoading, setBalanceLoading] = useState(true);
 
    <div className="topsection flex justify-between gap-[10px] background-[white] p-[10px] rounded-[10px] mb-[15px]">
   <div className=" producthistorts w-1/3">
-    <Balance  />
+   <Balance orders={orders} />
   </div>
   <div className=" producthistorts w-1/3">
     <HomeProducts />
